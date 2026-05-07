c6813926f39bf1af019837657a72aef103d74479
Baqir <Baqirhzaidi5@gmail.com>
2026-05-07 12:39:46 -0500

    initializing for deployment

--- main.py
+++ main.py
@@ -1,34 +1,60 @@
+import json
 from math import fabs
 from bs4 import BeautifulSoup
 import requests
-from datetime import datetime
+from datetime import date, datetime
+from flask import Flask, jsonify
 
-url = "https://www.urdupoint.com/islam/shia/dallas-prayer-timings.html"
+app = Flask(__name__)
 
-response = requests.get(url)
+def get_prayer_times():
+    url = "https://www.urdupoint.com/islam/shia/dallas-prayer-timings.html"
 
-html_content = response.text
-soup = BeautifulSoup(html_content, "html.parser")
-info = soup.find("table", class_= "main_timings_table names_tbl prayer_table ltr fs15 lh30 mb20")
-day = datetime.today()
-day_str = day.strftime("%d %B %Y")
-prayer_times = []
+    response = requests.get(url)
 
-for row in info.find_all("tr"):
-    cols = row.find_all("td")
-    if cols:
-        prayer_time = cols[0].get_text(strip=True)
-        prayer_times.append(prayer_time)
+    html_content = response.text
+    soup = BeautifulSoup(html_content, "html.parser")
+    info = soup.find("table", class_= "main_timings_table names_tbl prayer_table ltr fs15 lh30 mb20")
+    day = datetime.today()
+    day_str = day.strftime("%d %B %Y")
 
-finished_prayer_times = {
-    "Fajar" : prayer_times[0],
-    "Thuhr" : prayer_times[2],
-    "Asr" : prayer_times[3],
-    "Maghrib" : prayer_times[4],
-    "Isha" : prayer_times[5]
-}
+    prayer_times = []
 
-print(day_str)
+    for row in info.find_all("tr"):
+        cols = row.find_all("td")
+        if cols:
+            prayer_times.append(cols[0].get_text(strip=True))
+
+    finished_prayer_times = {
+        "Fajar" : prayer_times[0],
+        "Thuhr" : prayer_times[2],
+        "Asr" : prayer_times[3],
+        "Maghrib" : prayer_times[4],
+        "Isha" : prayer_times[5]
+    }
+
+    for prayer, time in finished_prayer_times.items():
+        finished_prayer_times[prayer] = datetime.strptime(time, "%H:%M").strftime("%-I:%M %p")
+
+
+    return {"date": day_str, "times": finished_prayer_times}
+
+print(get_prayer_times())
+
+
+result = get_prayer_times()
+
+@app.route('/')
+def prayer_api():
+    return jsonify(get_prayer_times())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
+
+
+
+
+
 
     
 
