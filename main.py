95d1d3a449ff1f53df875a622dba663f644f1366
Baqir <Baqirhzaidi5@gmail.com>
2026-05-07 13:29:34 -0500

    .

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-import json
-from math import fabs
 from bs4 import BeautifulSoup
 import requests
-from datetime import date, datetime
+from datetime import datetime
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-day = datetime.today()
-day_str = day.strftime("%d %B %Y")
 
 def get_prayer_times():
     url = "https://www.urdupoint.com/islam/shia/dallas-prayer-timings.html"
 
-    response = requests.get(url)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+    }
+
+    response = requests.get(url, headers=headers)
 
     html_content = response.text
     soup = BeautifulSoup(html_content, "html.parser")
     info = soup.find("table", class_= "main_timings_table names_tbl prayer_table ltr fs15 lh30 mb20")
+
+    if info is None:
+        return {"error": "Could not fetch prayer times", "status": response.status_code}
 
 
     prayer_times = []
@@ -34,17 +39,13 @@ def get_prayer_times():
         "Isha" : prayer_times[5]
     }
 
-    for prayer, time in finished_prayer_times.items():
-        finished_prayer_times[prayer] = datetime.strptime(time, "%H:%M").strftime("%-I:%M %p")
+    day_str = datetime.today().strftime("%d %B %Y")
 
+    for prayer, time in finished_prayer_times.items():
+        t = datetime.strptime(time, "%H:%M")
+        finished_prayer_times[prayer] = t.strftime("%I:%M %p").lstrip("0")
 
     return {"date": day_str, "times": finished_prayer_times}
-
-print(get_prayer_times())
-print(day_str)
-
-
-result = get_prayer_times()
 
 @app.route('/')
 def prayer_api():
