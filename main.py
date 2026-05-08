a9b0fdee5c6f87cab2e634bf50d97da7340bd7d9
Baqir <Baqirhzaidi5@gmail.com>
2026-05-08 15:49:35 -0500

    Used an api to help me calculate prayer times, fixed it as well

--- main.py
+++ main.py
@@ -1,79 +1,36 @@
-from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
-from flask import Flask, jsonify
-import os 
+from flask import Flask, request
 
-os.environ.get("19d52803af2986c9e4fbdd9664d46dd6")
 app = Flask(__name__)
 
+date = datetime.today().strftime("%d %B %Y") 
 
-def get_prayer_times():
-    url = "https://www.urdupoint.com/islam/shia/dallas-prayer-timings.html"
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
+def get_prayer_timings(): 
+    response = requests.get("https://api.aladhan.com/v1/timingsByCity?city=Dallas&country=US&method=0")
+    data = response.json()
+    timings = data["data"]["timings"]
+    timings_after_convert = {
+        "Fajar" : datetime.strptime(timings["Fajr"], "%H:%M").strftime("%I:%M %p").lstrip("0"),
+        "Thuhr" : datetime.strptime(timings["Dhuhr"], "%H:%M").strftime("%I:%M %p").lstrip("0"),
+        "Asr" : datetime.strptime(timings["Asr"], "%H:%M").strftime("%I:%M %p").lstrip("0"),
+        "Maghrib" : datetime.strptime(timings["Maghrib"], "%H:%M").strftime("%I:%M %p").lstrip("0"),
+        "Isha" : datetime.strptime(timings["Isha"], "%H:%M").strftime("%I:%M %p").lstrip("0"),
+        "Date" : datetime.today().strftime("%d %B %Y") 
     }
 
-    response = requests.get(url, headers=headers)
-
-    html_content = response.text
-    soup = BeautifulSoup(html_content, "html.parser")
-    info = soup.find("table", class_= "main_timings_table names_tbl prayer_table ltr fs15 lh30 mb20")
-
-    if info is None:
-        return {"error": "Could not fetch prayer times", "status": response.status_code}
-
-
-    prayer_times = []
-
-    for row in info.find_all("tr"):
-        cols = row.find_all("td")
-        if cols:
-            prayer_times.append(cols[0].get_text(strip=True))
-
-    finished_prayer_times = {
-        "Fajar" : prayer_times[0],
-        "Thuhr" : prayer_times[2],
-        "Asr" : prayer_times[3],
-        "Maghrib" : prayer_times[4],
-        "Isha" : prayer_times[5]
-    }
-
-    day_str = datetime.today().strftime("%d %B %Y")
-
-    for prayer, time in finished_prayer_times.items():
-        t = datetime.strptime(time, "%H:%M")
-        finished_prayer_times[prayer] = t.strftime("%I:%M %p").lstrip("0")
-
-    return {"date": day_str, "times": finished_prayer_times}
-
-
-
-print(get_prayer_times())
-@app.route('/')
-def prayer_api():
-    return jsonify(get_prayer_times())
-
-@app.route('/debug')
-def debug():
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
-    }
-    response = requests.get("https://www.urdupoint.com/islam/shia/dallas-prayer-timings.html", headers=headers)
-    return response.text
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    return timings_after_convert
 
 
 
 
 
 
+
+@app.route("/")
+def initialize():
+    return get_prayer_timings()
     
 
+if __name__ == "__main__":
+    app.run(debug=True)
